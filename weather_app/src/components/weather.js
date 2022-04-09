7de33b30501d0e2bd8ac990ad605d20313d5a5a4
Alex <maitrongtuanbmt@gmail.com>
2022-04-09 22:33:52 +0800

    Apply bootstrap template

--- weather_app/src/components/weather.js
+++ weather_app/src/components/weather.js
@@ -1,46 +1,58 @@
 import React from 'react';
+import './weather.css';
 
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes();
+var date = new Date().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })
 
 const Weather = ({ WeatherData }) => (
-    <section className="vh-100" style={{ backgroundColor: '#4B515D' }}>
-        <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-md-8 col-lg-6 col-xl-4">
-                    <div className="card" style={{ color: '#4B515D', borderRadius: '35px' }}>
-                        <div className="card-body p-4">
-                            <div className="d-flex">
-                                <h6 className="flex-grow-1">{WeatherData.name}</h6>
-                                <h6>{time}</h6>
-                            </div>
-                            <div className="d-flex flex-column text-center mt-5 mb-4">
-                                <h6 className="display-4 mb-0 font-weight-bold" style={{ color: '#1C2331' }}> {WeatherData.main.temp}°C </h6>
-                                <span className="small" style={{ color: '#868B94' }}>{WeatherData.weather[0].description}</span>
-                            </div>
-                            <div className="d-flex align-items-center">
-                                <div className="flex-grow-1" style={{ fontSize: '1rem' }}>
-                                    <div><i className="fas fa-wind fa-fw" style={{ color: '#868B94' }} /> <span className="ms-1"> {WeatherData.wind.speed} km/h </span></div>
-                                    <div><i className="fas fa-tint fa-fw" style={{ color: '#868B94' }} /> <span className="ms-1"> {WeatherData.main.humidity}% </span></div>
-                                </div>
-                                <div>
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp" width="100px" />
-                                </div>
-                            </div>
+    <div className="container-fluid px-1 px-sm-3 py-5 mx-auto">
+        <div className="row d-flex justify-content-center">
+            <div className="row card0">
+                <div className="card1 col-lg-8 col-md-7"> <small>the.weather</small>
+                    <div className="text-center"> <img className="image mt-5" src="https://i.imgur.com/M8VyA2h.png" /> </div>
+                    <div className="row px-3 mt-3 mb-3">
+                        <h1 className="large-font mr-3">{Math.floor(WeatherData.main.temp)}°</h1>
+                        <div className="d-flex flex-column mr-3">
+                            <h2 className="mt-3 mb-0">{WeatherData.name}</h2> <small>{time} - {date}</small>
                         </div>
+                        <div className="d-flex flex-column text-center">
+                            <h3 className="fa fa-sun-o mt-4" /> <small>{WeatherData.weather[0].main}</small>
+                        </div>
+                    </div>
+                </div>
+                <div className="card2 col-lg-4 col-md-5">
+                    <div className="row px-3"> <input type="text" name="location" placeholder="Another location" className="mb-5" />
+                        <div className="fa fa-search mb-5 mr-0 text-center" />
+                    </div>
+                    <div className="mr-5">
+                        <p className="light-text suggestion">Birmingham</p>
+                        <p className="light-text suggestion">Manchester</p>
+                        <p className="light-text suggestion">New York</p>
+                        <p className="light-text suggestion">California</p>
+                        <div className="line my-5" />
+                        <p>Weather Details</p>
+                        <div className="row px-3">
+                            <p className="light-text">Humidity</p>
+                            <p className="ml-auto">{WeatherData.main.humidity}%</p>
+                        </div>
+                        <div className="row px-3">
+                            <p className="light-text">Wind</p>
+                            <p className="ml-auto">{WeatherData.wind.speed} km/h</p>
+                        </div>
+                        <div className="row px-3">
+                            <p className="light-text">Sunrise</p>
+                            <p className="ml-auto">{new Date(WeatherData.sys.sunrise * 1000).toLocaleTimeString('en-us')}</p>
+                        </div>
+                        <div className="row px-3">
+                            <p className="light-text">Sunset</p>
+                            <p className="ml-auto">{new Date(WeatherData.sys.sunset * 1000).toLocaleTimeString('en-us')}</p>
+                        </div>
+                        <div className="line mt-3" />
                     </div>
                 </div>
             </div>
         </div>
-    </section>)
-{/* <div>
-         <h1>Location: {WeatherData.name}</h1>
-        <h2>Temperature: {WeatherData.main.temp}</h2>
-         <h2>Sunrise: {new Date(WeatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</h2>
-         <h2>Sunset: {new Date(WeatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</h2>
-         <h2>Description: {WeatherData.weather[0].description}</h2>
-     </div>  */}
-
-
+    </div>)
 
 export default Weather;
